1e91836d820b302e259e341c07eebcb782f5fb64
Oliver Harold <oliverhsj@hotmail.dk>
2024-02-27 18:15:19 +0700

    Update WorldModelSequentialTrainer.py

--- Trainers/WorldModelSequentialTrainer.py
+++ Trainers/WorldModelSequentialTrainer.py
@@ -147,7 +147,7 @@ class WorldModelSequentialTrainer(SequentialTrainer):
                 if terminated.any() or truncated.any():
                     states, infos = self.env.reset()
                     states = torch.Tensor(states).to(self.device)
-                    latent = self.world_model.to_latent()
+                    latent = self.world_model.to_latent(states)
                     h_state = self.world_model.initial_state()
                     enc_states = torch.concat([latent, h_state[0]], dim=-1)
                     
