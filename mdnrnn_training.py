9f55f12c4c456c1064b0fc3872470ef106c5caee
Oliver Harold <oliverhsj@hotmail.dk>
2024-03-04 22:47:19 +0700

    Update mdnrnn_training.py

--- mdnrnn_training.py
+++ mdnrnn_training.py
@@ -102,7 +102,7 @@ class GenerateCallback(L.Callback):
             trainer.logger.log_image(key="Reconstructions_Next_Step", images=[grid], step=trainer.global_step)
 
 def get_car_racing_dataset():
-    train_dataset = EpisodeDataset(DATASET_PATH, transform=transform, action_space=ACTION_SPACE, seq_length=SEQ_LENGTH, encoder=encoding_model.encoder)
+    train_dataset = EpisodeDataset(DATASET_PATH, transform=transform, action_space=ACTION_SPACE, seq_length=SEQ_LENGTH, encoder=encoding_model.encoder, device=DEVICE)
     train_set, val_set = torch.utils.data.random_split(train_dataset, [1-VAL_SPLIT, VAL_SPLIT])
 
     train_loader = data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=True, pin_memory=True, num_workers=NUM_WORKERS)
@@ -111,7 +111,7 @@ def get_car_racing_dataset():
     return train_loader, val_loader
 
 def get_seq_input_imgs(n=8):
-    dataset = EpisodeDataset(DATASET_PATH, transform=transform, action_space=ACTION_SPACE, seq_length=SEQ_LENGTH, encoder=encoding_model.encoder)
+    dataset = EpisodeDataset(DATASET_PATH, transform=transform, action_space=ACTION_SPACE, seq_length=SEQ_LENGTH, encoder=encoding_model.encoder, device=DEVICE)
     idx = np.random.randint(0, len(dataset), size=n)
     return transpose_2d([dataset.get_image_seq(i) for i in idx])
 
