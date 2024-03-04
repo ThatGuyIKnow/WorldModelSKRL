c215a21bf2aed1dc865d272cba56b18ed9d89c6f
Oliver Harold <oliverhsj@hotmail.dk>
2024-03-04 22:59:52 +0700

    Update mdnrnn_training.py

--- mdnrnn_training.py
+++ mdnrnn_training.py
@@ -103,11 +103,10 @@ class GenerateCallback(L.Callback):
 
 def get_car_racing_dataset():
     train_dataset = EpisodeDataset(DATASET_PATH, transform=transform, action_space=ACTION_SPACE, seq_length=SEQ_LENGTH, encoder=encoding_model.encoder, device=DEVICE)
-    train_dataset.train_data.to(DEVICE)
     train_set, val_set = torch.utils.data.random_split(train_dataset, [1-VAL_SPLIT, VAL_SPLIT])
 
-    train_loader = data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=True, pin_memory=True, num_workers=NUM_WORKERS)
-    val_loader = data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False, drop_last=False, num_workers=NUM_WORKERS)
+    train_loader = data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=True, pin_memory=True, num_workers=NUM_WORKERS, multiprocessing_context='spawn')
+    val_loader = data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False, drop_last=False, num_workers=NUM_WORKERS, multiprocessing_context='spawn')
 
     return train_loader, val_loader
 
