c2f0e3f6e75795bebe4dbad3353b4850d0e80293
Oliver Harold <oliverhsj@hotmail.dk>
2024-03-10 14:00:26 +0700

    Update wm_training.py

--- wm_training.py
+++ wm_training.py
@@ -77,7 +77,7 @@ class GenerateCallback(L.Callback):
             state_img = torch.concat([v for v in states.swapaxes(0, 1)], dim=-2)
             imgs = torch.concat([state_img, reconst_obs, reconst_post_obs], dim=-1)
             imgs = imgs.unsqueeze(1).cpu().numpy()
-            trainer.logger.log_video({"video": wandb.Video(imgs, fps=30)})
+            trainer.logger.log({"video": wandb.Video(imgs, fps=30)})
 
 
 def train_world_model():
